77cfd1b35e69a3d57c6c6ff0f117799ec109ca36
hiMinou <66796740+hiMinou@users.noreply.github.com>
2021-04-16 05:49:30 +0200

    ajout du menu hamburger de la version mobile

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*____afficher le menu____*/
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+  nav =  document.getElementById(navId)
+
+  if(toggle && nav){
+    toggle.addEventListener('click', ()=>{
+      nav.classList.toggle('show')
+    })
+  }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*____changer l'onglet active____*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+  //Active link
+  navLink.forEach( n => n.classList.remove('active'))
+  this.classList.add('active')
+
+  //Faire disparaitre me menu
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
