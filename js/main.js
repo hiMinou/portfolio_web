7abbb9398fa789bfd0e9eaa962e384e4f799b793
hiMinou <66796740+hiMinou@users.noreply.github.com>
2021-04-18 22:06:30 +0200

    ajout des differnet layout grand-moyen-petit ecran

--- js/main.js
+++ js/main.js
@@ -25,3 +25,37 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*____gestion des animations avec scrollreveal____*/
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+/*____animations home____*/
+sr.reveal('.home__title',{})
+sr.reveal('.button',{delay:150})
+sr.reveal('.home__img',{delay: 300})
+sr.reveal('.home__social-icon',{interval: 200})
+
+
+/*____animations A propos____*/
+sr.reveal('.about__img',{})
+sr.reveal('.about__subtitle',{delay:150})
+sr.reveal('.abou__text',{delay:150})
+
+/*____animations  skills____*/
+sr.reveal('.skills__subtitle',{})
+sr.reveal('.skills__text',{delay:150})
+sr.reveal('.skills__text',{interval:200})
+sr.reveal('.skills__img',{delay:150})
+
+/*____animations  projets____*/
+
+sr.reveal('.work__img',{interval:200})
+/*____animations  projets____*/
+sr.reveal('.contact__input',{interval:200})
