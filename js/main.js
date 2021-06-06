68327fb4dd6c8cddd67427a041aa2bec277c3a11
hiMinou <minou.matoko.officel@gmail.com>
2021-06-06 20:30:56 +0200

    ajouts modifications layout

---
+++ js/main.js
@@ -0,0 +1,61 @@
+/*____afficher le menu____*/
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+  nav =  document.getElementById(navId)
+
+  if(toggle && nav){
+    toggle.addEventListener('click', ()=>{
+      nav.classList.toggle('show')
+    })
+  }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*____changer l'onglet active____*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+  //Active link
+  navLink.forEach( n => n.classList.remove('active'))
+  this.classList.add('active')
+
+  //Faire disparaitre me menu
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*____gestion des animations avec scrollreveal____*/
+
+/*const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+/*____animations home____*/
+/*sr.reveal('.home__title',{})
+sr.reveal('.button',{delay:50})
+sr.reveal('.home__img',{delay: 150})
+sr.reveal('.home__social-icon',{interval: 150})
+
+
+/*____animations A propos____*/
+/*sr.reveal('.about__img',{delay:25})
+sr.reveal('.about__subtitle',{delay:50})
+sr.reveal('.abou__text',{delay:50})
+
+/*____animations  skills____*/
+/*sr.reveal('.skills__subtitle',{})
+sr.reveal('.skills__text',{delay:50})
+sr.reveal('.skills__text',{interval:150})
+sr.reveal('.skills__img',{delay:50})
+
+/*____animations  projets____*/
+
+/*sr.reveal('.work__img',{interval:100})
+/*____animations  projets____*/
+/*sr.reveal('.contact__input',{interval:50})*/
